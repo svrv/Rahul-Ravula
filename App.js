e029426c86463443765f5d1d22b20a62d72ceeb3
Rahul Ravula <91576414+svrv@users.noreply.github.com>
2021-11-09 00:05:33 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.css'
+import Main from './components/Main'
+
+function App() {
+  return (
+    <div className="m-2 h-screen max-h-screen">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
